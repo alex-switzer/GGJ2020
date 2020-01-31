8464a7bab44d564eab5a111196b84a73c0810373
daedricreign <daedricreign@gmail.com>
2020-01-31 21:50:52 +1300

    Help
    Signed-off-by: daedricreign <daedricreign@gmail.com>

--- game.js
+++ game.js
@@ -1,18 +1,49 @@
 
 window.addEventListener("load", loadPage); 
 
+words = "You must take life the way it comes at you and make the best of it";
+cursor = 0;
+
 function loadPage() {
 
     var gameText = document.getElementById("gameText");
     var inputText = document.getElementById("inputText")
 
-    inputText.addEventListener("keyup", keyPressed); 
-
-}
-
-function keyPressed(events) {
+    //console.log(readTextFile("file:///C:\\Users\\GameJam\\Desktop\\GGJ\\texts.txt"));
     
-    console.log(typeof sdf);
+
+    gameText.innerText = words;
+    inputText.addEventListener("keypress", keyPressed); 
 
 }
 
+function keyPressed(events) {    
+
+    if(events.key == words.substring(cursor,cursor+1)){
+        cursor++;
+        if(events.keyCode == 32){
+            events.target.value = '';
+        }
+    }
+    
+    gameText.innerText = words.slice(0, cursor) + "|" + words.slice(cursor);
+
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
