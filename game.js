6dfd02d48847775cc04b82ea9558a233d5ab03f2
daedricreign <daedricreign@gmail.com>
2020-01-31 20:44:44 +1300

    Signed-off-by: daedricreign <daedricreign@gmail.com>

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
 
-//TODO: put game here
+window.addEventListener("load", loadPage); 
+
+function loadPage() {
+
+    var gameText = document.getElementById("gameText");
+    var inputText = document.getElementById("inputText")
+
+    inputText.addEventListener("keyup", keyPressed); 
+
+}
+
+function keyPressed(events) {
+    
+    console.log(typeof sdf);
+
+}
 
